1c9c19f344fbcba62d7860634e7a222882a71a6f
jade <jyf20112061@163.com>
2016-10-12 15:55:38 +0800

    二维码生成器

--- src/main/java/com/web/myapp/demo/qrcode/QRCodeGenerator.java
+++ src/main/java/com/web/myapp/demo/qrcode/QRCodeGenerator.java
@@ -2,207 +2,310 @@ package com.web.myapp.demo.qrcode;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
- 
+
 import javax.imageio.ImageIO;
- 
+
 import jp.sourceforge.qrcode.QRCodeDecoder;
 import jp.sourceforge.qrcode.exception.DecodingFailedException;
- 
+
 import com.swetake.util.Qrcode;
- 
-/**   
+
+/**
  * 二维码生成器
  */
 public class QRCodeGenerator {
-	
-	/** 
-     * 生成二维码(QRCode)图片 
-     * @param content 存储内容 
-     * @param imgPath 图片路径 
-     */
-    public void encoderQRCode(String content, String imgPath) {  
-        this.encoderQRCode(content, imgPath, "png", 7);  
-    }  
-       
-    /** 
-     * 生成二维码(QRCode)图片 
-     * @param content 存储内容 
-     * @param output 输出流 
-     */ 
-    public void encoderQRCode(String content, OutputStream output) {  
-        this.encoderQRCode(content, output, "png", 7);  
-    }  
-       
-    /** 
-     * 生成二维码(QRCode)图片 
-     * @param content 存储内容 
-     * @param imgPath 图片路径 
-     * @param imgType 图片类型 
-     */ 
-    public void encoderQRCode(String content, String imgPath, String imgType) {  
-        this.encoderQRCode(content, imgPath, imgType, 7);  
-    }  
-       
-    /** 
-     * 生成二维码(QRCode)图片 
-     * @param content 存储内容 
-     * @param output 输出流 
-     * @param imgType 图片类型 
-     */ 
-    public void encoderQRCode(String content, OutputStream output, String imgType) {  
-        this.encoderQRCode(content, output, imgType, 7);  
-    }  
-   
-    /** 
-     * 生成二维码(QRCode)图片 
-     * @param content 存储内容 
-     * @param imgPath 图片路径 
-     * @param imgType 图片类型 
-     * @param size 二维码尺寸 
-     */ 
-    public void encoderQRCode(String content, String imgPath, String imgType, int size) {  
-        try {  
-            BufferedImage bufImg = this.qRCodeCommon(content, imgType, size);  
-               
-            File imgFile = new File(imgPath);  
-            // 生成二维码QRCode图片  
-            ImageIO.write(bufImg, imgType, imgFile);  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-    }  
-   
-    /** 
-     * 生成二维码(QRCode)图片 
-     * @param content 存储内容 
-     * @param output 输出流 
-     * @param imgType 图片类型 
-     * @param size 二维码尺寸 
-     */ 
-    public void encoderQRCode(String content, OutputStream output, String imgType, int size) {  
-        try {  
-            BufferedImage bufImg = this.qRCodeCommon(content, imgType, size);  
-            // 生成二维码QRCode图片  
-            ImageIO.write(bufImg, imgType, output);  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-    }  
-       
-    /** 
-     * 生成二维码(QRCode)图片的公共方法 
-     * @param content 存储内容 
-     * @param imgType 图片类型 
-     * @param size 二维码尺寸 
-     * @return 
-     */ 
-    private BufferedImage qRCodeCommon(String content, String imgType, int size) {  
-        BufferedImage bufImg = null;  
-        try {  
-            Qrcode qrcodeHandler = new Qrcode();  
-            // 设置二维码排错率，可选L(7%)、M(15%)、Q(25%)、H(30%)，排错率越高可存储的信息越少，但对二维码清晰度的要求越小  
-            qrcodeHandler.setQrcodeErrorCorrect('M');  
-            qrcodeHandler.setQrcodeEncodeMode('B');  
-            // 设置设置二维码尺寸，取值范围1-40，值越大尺寸越大，可存储的信息越大  
-            qrcodeHandler.setQrcodeVersion(size);  
-            // 获得内容的字节数组，设置编码格式  
-            byte[] contentBytes = content.getBytes("utf-8");  
-            // 图片尺寸  
-            int imgSize = 67 + 12 * (size - 1);  
-            bufImg = new BufferedImage(imgSize, imgSize, BufferedImage.TYPE_INT_RGB);  
-            Graphics2D gs = bufImg.createGraphics();  
-            // 设置背景颜色  
-            gs.setBackground(Color.WHITE);  
-            gs.clearRect(0, 0, imgSize, imgSize);  
-   
-            // 设定图像颜色> BLACK  
-            gs.setColor(Color.BLACK);  
-            // 设置偏移量，不设置可能导致解析出错  
-            int pixoff = 2;  
-            // 输出内容> 二维码  
-            if (contentBytes.length > 0 && contentBytes.length < 800) {  
-                boolean[][] codeOut = qrcodeHandler.calQrcode(contentBytes);  
-                for (int i = 0; i < codeOut.length; i++) {  
-                    for (int j = 0; j < codeOut.length; j++) {  
-                        if (codeOut[j][i]) {  
-                            gs.fillRect(j * 3 + pixoff, i * 3 + pixoff, 3, 3);  
-                        }  
-                    }  
-                }  
-            } else {  
-                throw new Exception("QRCode content bytes length = " + contentBytes.length + " not in [0, 800].");  
-            }  
-            gs.dispose();  
-            bufImg.flush();  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-        return bufImg;  
-    }  
-       
-    /** 
-     * 解析二维码（QRCode） 
-     * @param imgPath 图片路径 
-     * @return 
-     */ 
-    public String decoderQRCode(String imgPath) {  
-        // QRCode 二维码图片的文件  
-        File imageFile = new File(imgPath);  
-        BufferedImage bufImg = null;  
-        String content = null;  
-        try {  
-            bufImg = ImageIO.read(imageFile);  
-            QRCodeDecoder decoder = new QRCodeDecoder();  
-            content = new String(decoder.decode(new QRCodeImg(bufImg)), "utf-8");   
-        } catch (IOException e) {  
-            System.out.println("Error: " + e.getMessage());  
-            e.printStackTrace();  
-        } catch (DecodingFailedException dfe) {  
-            System.out.println("Error: " + dfe.getMessage());  
-            dfe.printStackTrace();  
-        }  
-        return content;  
-    }  
-       
-    /** 
-     * 解析二维码（QRCode） 
-     * @param input 输入流 
-     * @return 
-     */ 
-    public String decoderQRCode(InputStream input) {  
-        BufferedImage bufImg = null;  
-        String content = null;  
-        try {  
-            bufImg = ImageIO.read(input);  
-            QRCodeDecoder decoder = new QRCodeDecoder();  
-            content = new String(decoder.decode(new QRCodeImg(bufImg)), "utf-8");   
-        } catch (IOException e) {  
-            System.out.println("Error: " + e.getMessage());  
-            e.printStackTrace();  
-        } catch (DecodingFailedException dfe) {  
-            System.out.println("Error: " + dfe.getMessage());  
-            dfe.printStackTrace();  
-        }  
-        return content;  
-    }  
-   
-    public static void main(String[] args) {  
-        String imgPath = "d:/temp/test.png";  
-        String encoderContent = "http://www.baidu.com";  
-        QRCodeGenerator generator = new QRCodeGenerator();  
-        generator.encoderQRCode(encoderContent, imgPath, "png");  
-        System.out.println("========encoder success");  
-           
-           
-        String decoderContent = generator.decoderQRCode(imgPath);  
-        System.out.println("解析结果如下：");  
-        System.out.println(decoderContent);  
-        System.out.println("========decoder success!!!");  
-    }
-       
+	public static final int QRCODE_SIZE = 7;
+	public static final int LOGO_SIZE = 60;
+	public static final String IMAGE_TYPE = "png";
+
+	/**
+	 * 生成二维码(QRCode)图片
+	 * 
+	 * @param content
+	 *            存储内容
+	 * @param imgPath
+	 *            图片路径
+	 */
+	public void encoderQRCode(String content, String imgPath) {
+		this.encoderQRCode(content, imgPath, IMAGE_TYPE, QRCODE_SIZE);
+	}
+
+	/**
+	 * 生成二维码(QRCode)图片
+	 * 
+	 * @param content
+	 *            存储内容
+	 * @param output
+	 *            输出流
+	 */
+	public void encoderQRCode(String content, OutputStream output) {
+		this.encoderQRCode(content, output, IMAGE_TYPE, QRCODE_SIZE);
+	}
+
+	/**
+	 * 生成二维码(QRCode)图片
+	 * 
+	 * @param content
+	 *            存储内容
+	 * @param imgPath
+	 *            图片路径
+	 * @param imgType
+	 *            图片类型
+	 */
+	public void encoderQRCode(String content, String imgPath, String imgType) {
+		this.encoderQRCode(content, imgPath, imgType, QRCODE_SIZE);
+	}
+
+	/**
+	 * 生成二维码(QRCode)图片
+	 * 
+	 * @param content
+	 *            存储内容
+	 * @param output
+	 *            输出流
+	 * @param imgType
+	 *            图片类型
+	 */
+	public void encoderQRCode(String content, OutputStream output,
+			String imgType) {
+		this.encoderQRCode(content, output, imgType, QRCODE_SIZE);
+	}
+
+	/**
+	 * 生成二维码(QRCode)图片
+	 * 
+	 * @param content
+	 *            存储内容
+	 * @param imgPath
+	 *            图片路径
+	 * @param imgType
+	 *            图片类型
+	 * @param size
+	 *            二维码尺寸
+	 */
+	public void encoderQRCodeWithLogo(String content, String imgPath,
+			String imgType, int size, String logoPath) {
+		try {
+			BufferedImage bufImg = this.qRCodeCommonWithLogo(content, imgType,
+					size, logoPath);
+			File imgFile = new File(imgPath);
+			// 生成二维码QRCode图片
+			ImageIO.write(bufImg, imgType, imgFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 生成二维码(QRCode)图片
+	 * 
+	 * @param content
+	 *            存储内容
+	 * @param imgPath
+	 *            图片路径
+	 * @param imgType
+	 *            图片类型
+	 * @param size
+	 *            二维码尺寸
+	 */
+	public void encoderQRCode(String content, String imgPath, String imgType,
+			int size) {
+		try {
+			BufferedImage bufImg = this.qRCodeCommon(content, imgType, size);
+			File imgFile = new File(imgPath);
+			// 生成二维码QRCode图片
+			ImageIO.write(bufImg, imgType, imgFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 生成二维码(QRCode)图片
+	 * 
+	 * @param content
+	 *            存储内容
+	 * @param output
+	 *            输出流
+	 * @param imgType
+	 *            图片类型
+	 * @param size
+	 *            二维码尺寸
+	 */
+	public void encoderQRCode(String content, OutputStream output,
+			String imgType, int size) {
+		try {
+			BufferedImage bufImg = this.qRCodeCommon(content, imgType, size);
+			// 生成二维码QRCode图片
+			ImageIO.write(bufImg, imgType, output);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 生成中间带有logo的二维码(QRCode)图片的公共方法
+	 * 
+	 * @param content
+	 *            存储内容
+	 * @param imgType
+	 *            图片类型
+	 * @param size
+	 *            二维码尺寸
+	 */
+	private BufferedImage qRCodeCommonWithLogo(String content, String imgType,
+			int size, String logoPath) {
+		BufferedImage bufImg = qRCodeCommon(content, imgType, size);
+		return addLogoToQRCode(bufImg, logoPath);
+	}
+
+	/**
+	 * 生成二维码(QRCode)图片的公共方法
+	 * 
+	 * @param content
+	 *            存储内容
+	 * @param imgType
+	 *            图片类型
+	 * @param size
+	 *            二维码尺寸
+	 */
+	private BufferedImage qRCodeCommon(String content, String imgType, int size) {
+		BufferedImage bufImg = null;
+		try {
+			Qrcode qrcodeHandler = new Qrcode();
+			// 设置二维码排错率，可选L(7%)、M(15%)、Q(25%)、H(30%)，排错率越高可存储的信息越少，但对二维码清晰度的要求越小
+			qrcodeHandler.setQrcodeErrorCorrect('M');
+			qrcodeHandler.setQrcodeEncodeMode('B');
+			// 设置设置二维码尺寸，取值范围1-40，值越大尺寸越大，可存储的信息越大
+			qrcodeHandler.setQrcodeVersion(size);
+			// 获得内容的字节数组，设置编码格式
+			byte[] contentBytes = content.getBytes("utf-8");
+			// 图片尺寸
+			int imgSize = 67 + 12 * (size - 1);
+			bufImg = new BufferedImage(imgSize, imgSize,
+					BufferedImage.TYPE_INT_RGB);
+			Graphics2D gs = bufImg.createGraphics();
+			// 设置背景颜色
+			gs.setBackground(Color.WHITE);
+			gs.clearRect(0, 0, imgSize, imgSize);
+			// 设定图像颜色> BLACK
+			gs.setColor(Color.BLACK);
+			// 设置偏移量，不设置可能导致解析出错
+			int pixoff = 2;
+			// 输出内容> 二维码
+			if (contentBytes.length > 0 && contentBytes.length < 800) {
+				boolean[][] codeOut = qrcodeHandler.calQrcode(contentBytes);
+				for (int i = 0; i < codeOut.length; i++) {
+					for (int j = 0; j < codeOut.length; j++) {
+						if (codeOut[j][i]) {
+							gs.fillRect(j * 3 + pixoff, i * 3 + pixoff, 3, 3);
+						}
+					}
+				}
+			} else {
+				throw new Exception("QRCode content bytes length = "
+						+ contentBytes.length + " not in [0, 800].");
+			}
+			gs.dispose();
+			bufImg.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return bufImg;
+	}
+
+	/**
+	 * 解析二维码（QRCode）
+	 * 
+	 * @param imgPath
+	 *            图片路径
+	 * @return
+	 */
+	public String decoderQRCode(String imgPath) {
+		// QRCode 二维码图片的文件
+		File imageFile = new File(imgPath);
+		BufferedImage bufImg = null;
+		String content = null;
+		try {
+			bufImg = ImageIO.read(imageFile);
+			QRCodeDecoder decoder = new QRCodeDecoder();
+			content = new String(decoder.decode(new QRCodeImg(bufImg)), "utf-8");
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
+		} catch (DecodingFailedException dfe) {
+			System.out.println("Error: " + dfe.getMessage());
+			dfe.printStackTrace();
+		}
+		return content;
+	}
+
+	/**
+	 * 解析二维码（QRCode）
+	 * 
+	 * @param input
+	 *            输入流
+	 */
+	public String decoderQRCode(InputStream input) {
+		BufferedImage bufImg = null;
+		String content = null;
+		try {
+			bufImg = ImageIO.read(input);
+			QRCodeDecoder decoder = new QRCodeDecoder();
+			content = new String(decoder.decode(new QRCodeImg(bufImg)), "utf-8");
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
+		} catch (DecodingFailedException dfe) {
+			System.out.println("Error: " + dfe.getMessage());
+			dfe.printStackTrace();
+		}
+		return content;
+	}
+
+	/**
+	 * 在二维码中间加入logo图片
+	 * 
+	 * @param bugImg
+	 * @param logoPath
+	 */
+	public static BufferedImage addLogoToQRCode(BufferedImage bufImg,
+			String logoPath) {
+		Image img = null;
+		try {
+			img = ImageIO.read(new File(logoPath));
+			Graphics2D g = bufImg.createGraphics();
+			// 获取bufImg的中间位置
+			int centerX = bufImg.getMinX() + bufImg.getWidth() / 2 - LOGO_SIZE
+					/ 2;
+			int centerY = bufImg.getMinY() + bufImg.getHeight() / 2 - LOGO_SIZE
+					/ 2;
+			g.drawImage(img, centerX, centerY, LOGO_SIZE, LOGO_SIZE, null);
+			g.dispose();
+			bufImg.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return bufImg;
+	}
+
+	public static void main(String[] args) {
+		String imgPath = "d:/temp/test.png";
+		String encoderContent = "http://www.baidu.com";
+		QRCodeGenerator generator = new QRCodeGenerator();
+		generator.encoderQRCode(encoderContent, imgPath, IMAGE_TYPE);
+		System.out.println("========encoder success");
+
+		String decoderContent = generator.decoderQRCode(imgPath);
+		System.out.println("解析结果如下：");
+		System.out.println(decoderContent);
+		System.out.println("========decoder success!!!");
+	}
+
 }
